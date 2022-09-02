e9c288ed80dd4858ed98b37629fcc6f88e31490d
Jalome Chirwa <click2sure@Jalomes-Air.home>
2022-09-02 21:31:34 +0200

    updated readme file

--- README.md
+++ README.md
@@ -1,9 +1,12 @@
 # Swift-fundamentals
 
 
-- [X] Variables
+- [X] Variables, properties and computed properties
 - [X] Constants
 - [x] Data types
 - [x] Functions
 - [x] Structures
 - [x] Instances
+- [x] Structures AKA struct 
+(Structures are meant to represent a specific functionality or  crucial component of your app i.e a screen, a request service etc.)
+
--- SwiftPlayGround.playground/Contents.swift
+++ SwiftPlayGround.playground/Contents.swift
@@ -14,6 +14,35 @@ print(name)
 func myFirstFunction(a:Int,b:Int) -> Int {
     return a + b
 }
-
-
 print(myFirstFunction(a:10,b:20))
+
+
+//declaring a structure and format
+struct MyStruct{
+//    variables and constants
+    var message:String = "" // variables and constants are considered properties of a structure
+    
+//    declaring a computed property
+    var computedMessage:String{
+        return "Jalome Says " + message
+    }
+    
+//    methods, functions are considered methods of a structure
+    func sendChatMessage(){
+        print(computedMessage)
+    }
+    
+    
+//    access levels
+    private func functionNotAccessible(){
+        
+    }
+    
+}
+
+
+//  instantiate a structure
+var structInstance = MyStruct()
+structInstance.message = "Hello world"
+structInstance.sendChatMessage()
+
