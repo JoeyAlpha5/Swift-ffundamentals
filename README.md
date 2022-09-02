6f465b978c8cbb6902721cb2a31145ffd0bcbdf7
Jalome Chirwa <click2sure@Jalomes-Air.home>
2022-09-02 20:13:47 +0200

    added readme file

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Swift-ffundamentals
+
+
+- [X] Variables
+- [X] Constants
+- [x] Data types
+- [x] Functions
+- [x] Structures
+- [x] Instances
---
+++ SwiftPlayGround.playground/Contents.swift
@@ -0,0 +1,19 @@
+//variables in swift
+var greeting:String = "Hello, playground"
+print(greeting)
+
+var myInt:Int = 100;
+print(myInt)
+
+//constants in swift
+let name:String = "Hello Jalome"
+print(name)
+
+
+//functions in swift
+func myFirstFunction(a:Int,b:Int) -> Int {
+    return a + b
+}
+
+
+print(myFirstFunction(a:10,b:20))
